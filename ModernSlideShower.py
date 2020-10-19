299d971ffe3def36543794fae344a3434b701525
FuhrerStein <zheludko@i.ua>
2020-10-19 20:38:11 +0300

    handle jpegtran absence update picture gl program in main py file

--- ModernSlideShower.py
+++ ModernSlideShower.py
@@ -34,6 +34,7 @@ import random
 #    todo: filtering image with glsl code
 #    todo: support for large images by splitting them into smaller subtextures
 
+#    todo+: if jpegtan was not found, do not show message to save rotation
 #    todo+: sticking of enable/disable levels key
 #    todo+: swap levels adjustments: put gamma min and max, adjust gamma with l+r mouse
 #    todo+: do not apply borders when apply levels
@@ -133,22 +134,28 @@ uniform vec2 pix_size;
 uniform vec2 wnd_size;
 uniform float zoom_scale;
 uniform float angle;
+uniform float show_amount;
+uniform float transparency;
 uniform bool one_by_one;
 
 out vec2 uv0;
-out float zoom_frag;
+out float translucency;
+out float f_show_amount;
+out float min_edge;
+out float max_edge;
+
 
 void main() {
 
-    float M_PI = 3.14159265358;
-    float pi_angle = angle * M_PI / 180;
-    mat2 rotate_mat = mat2(cos(pi_angle), - sin(pi_angle), sin(pi_angle), cos(pi_angle));
+    mat2 rotate_mat = mat2(cos(angle), - sin(angle), sin(angle), cos(angle));
     mat2 pix_size_mat = mat2(pix_size.x, 0, 0, pix_size.y);
     mat2 wnd_size_mat = mat2(1 / wnd_size.x, 0, 0, 1 / wnd_size.y);
     gl_Position = vec4((in_position * pix_size_mat * rotate_mat + displacement) * zoom_scale * wnd_size_mat, 0, 1);
-//    gl_Position = vec4((in_position * pix_size_mat * rotate_mat + displacement) * zoom_scale * wnd_size_mat, in_position.y, 1.5 - in_position.y / 2);
     uv0 = in_texcoord;
-    zoom_frag = zoom_scale;
+    translucency = 1 - smoothstep(.5, .7, transparency);
+    f_show_amount = smoothstep(0, .7, show_amount);
+    min_edge = 2.5 * (smoothstep(0, 1, show_amount)) + show_amount;
+    max_edge = 2 * (smoothstep(.7, 1, show_amount)) + show_amount;
 
     if (one_by_one)
     {
@@ -163,18 +170,23 @@ void main() {
 
 layout(binding=5) uniform sampler2D texture0;
 layout(binding=6) uniform sampler2D texture_curve;
-//layout(binding=6) uniform mat(256, 5) texture_curve;
 layout(binding=7, r32ui) uniform uimage2D histogram_texture;
 uniform bool useCurves;
 uniform bool count_histograms;
+uniform vec2 transition_center;
+uniform float zoom_scale;
 
 out vec4 fragColor;
 in vec2 uv0;
-in float zoom_frag;
+in float translucency;
+in float f_show_amount;
+in float min_edge;
+in float max_edge;
+
+uniform float hide_borders;
 
 void main() {
-//    vec4 tempColor = texture(texture0, uv0);
-    vec4 tempColor = textureLod(texture0, uv0, - log(zoom_frag));
+    vec4 tempColor = textureLod(texture0, uv0, - log(zoom_scale));
 
     if(useCurves)
     {
@@ -183,6 +195,7 @@ void main() {
         tempColor.g = texelFetch(texture_curve, ivec2((tempColor.g * 255 + .5), 2), 0).r;
         tempColor.b = texelFetch(texture_curve, ivec2((tempColor.b * 255 + .5), 3), 0).r;
 
+        // global curves
         tempColor.r = texelFetch(texture_curve, ivec2((tempColor.r * 255 + .5), 0), 0).r;
         tempColor.g = texelFetch(texture_curve, ivec2((tempColor.g * 255 + .5), 0), 0).r;
         tempColor.b = texelFetch(texture_curve, ivec2((tempColor.b * 255 + .5), 0), 0).r;
@@ -196,12 +209,19 @@ void main() {
         imageAtomicAdd(histogram_texture, ivec2(gray_value, 0), 1u);
 
         // red, green and blue histograms
+        // todo: use fma()
         imageAtomicAdd(histogram_texture, ivec2(tempColor.r * 255 + .5, 2), 1u);
         imageAtomicAdd(histogram_texture, ivec2(tempColor.g * 255 + .5, 3), 1u);
         imageAtomicAdd(histogram_texture, ivec2(tempColor.b * 255 + .5, 4), 1u);
     }
 
-    fragColor = tempColor;
+    // Edges and transitions
+    float to_edge_x = pow(smoothstep(-0.5, -.5 + hide_borders, -abs(uv0.x - .5)), .3);
+    float to_edge_y = pow(smoothstep(-0.5, -.5 + hide_borders, -abs(uv0.y - .5)), .3);
+    float to_edge = smoothstep(0, 1, to_edge_x + to_edge_y) * smoothstep(0, 1,  to_edge_x * to_edge_y);
+    float tran_alpha = smoothstep(-min_edge, -max_edge, -length(uv0 - transition_center)) * f_show_amount;
+
+    fragColor = vec4(tempColor.rgb, tran_alpha * translucency * to_edge);
 }
 
 #endif
@@ -222,7 +242,9 @@ class ModernSlideShower(mglw.WindowConfig):
     save_folder = ".\\SaveFolder\\"
     image_file_types = ('jpg', 'png', 'jpeg', 'gif', 'tif', 'tiff')
     list_file_type = 'sldlist'
-    jpegtran_exe = "c:\\Soft\\Programs\\libjpeg-turbo64\\bin\\jpegtran.exe"
+    jpegtran_exe_path = "c:\\Soft\\Programs\\libjpeg-turbo64\\bin"
+    jpegtran_exe_file = "jpegtran.exe"
+    jpegtran_exe = os.path.join(jpegtran_exe_path, jpegtran_exe_file)
     jpegtran_options = ' -optimize -rotate {0} -trim -copy all -outfile "{1}" "{1}"'
     image_list = []
     image_count = 0
@@ -453,8 +475,8 @@ class ModernSlideShower(mglw.WindowConfig):
 
     def find_jpegtran(self):
         if not os.path.isfile(self.jpegtran_exe):
-            if os.path.isfile(".\\jpegtran.exe"):
-                self.jpegtran_exe = os.path.abspath(".\\jpegtran.exe")
+            if os.path.isfile(self.jpegtran_exe_file):
+                self.jpegtran_exe = os.path.abspath(self.jpegtran_exe_file)
             else:
                 self.jpegtran_exe = None
 
@@ -723,7 +745,7 @@ class ModernSlideShower(mglw.WindowConfig):
         self.move_image()
 
         if self.pic_angle_future % 360:  # not at original angle
-            if not (self.pic_angle_future % 90):  # but at 90°-ish
+            if not (self.pic_angle_future % 90) and self.jpegtran_exe:  # but at 90°-ish
                 self.schedule_pop_message(2, duration=8000000, angle=360 - self.pic_angle_future % 360)
         else:
             self.unschedule_pop_message(2)
